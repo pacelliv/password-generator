3782d7f455f345f0bc9ffc715c61231e691be401
PacelliV <flores.eugenio03@gmail.com>
2022-10-02 12:19:14 -0500

    refactor code

--- README.md
+++ README.md
@@ -1,9 +1,43 @@
 # Password Generator
 
-## About the project
+## Overview
 
-Clicking the Generate button creates a set of two different passwords made up of 15 alphanumeric and special characters. To create a new set of passwords, click the Update button to clear the fields. Click on the passwords to copy them to your clipboard.
+The user will be able to:
 
-- [Github](https://github.com/PacelliV)
+-   Generate two passwords of 15 long alphanumeric characters.
+-   The passwords can be copied to the clipboard by clicking on them, an animation with displayed with a "Copied!" message.
 
-Happy Coding!
+This project was designed to work on small and big screens.
+
+## Screenshots
+
+<div>
+    <img src="screenshots/small-screen.JPG">
+    <img src="screenshots/big-screen.JPG">
+</div>
+
+## New concepts learned and applied
+
+-   [Variable declaration](https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/Variables)
+    -   [types of variables](https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/Variables#variable_types)
+    -   [let](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/let)
+    -   [const](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/const)
+-   [Function](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Functions)
+-   [Loops and iteration](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration)
+-   [Return statement](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/return)
+-   [Document.getElementById()](https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementById)
+-   [Node.appendChild()](https://developer.mozilla.org/en-US/docs/Web/API/Node/appendChild)
+-   [Element.classList](https://developer.mozilla.org/en-US/docs/Web/API/Element/classList)
+-   [Math.random()](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random)
+-   [Math.floor()](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/floor)
+-   [Element.remove()](https://developer.mozilla.org/en-US/docs/Web/API/Element/remove)
+-   [@keyframes](https://developer.mozilla.org/en-US/docs/Web/CSS/@keyframes)
+-   [@media](https://developer.mozilla.org/en-US/docs/Web/CSS/@media)
+
+## Acknowledgements
+
+I acknowledge Scrimba for teaching me the necessary tools to complete this project in my journey to become a full stack developer.
+
+## Thank you!🎉 🎉
+
+I hope you like this project and it ends up being useful to you 👨‍💻
--- index.js
+++ index.js
@@ -1,65 +1,139 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
-
-let passwordOne = [];
-let passwordTwo = [];
+const characters = [
+	"A",
+	"B",
+	"C",
+	"D",
+	"E",
+	"F",
+	"G",
+	"H",
+	"I",
+	"J",
+	"K",
+	"L",
+	"M",
+	"N",
+	"O",
+	"P",
+	"Q",
+	"R",
+	"S",
+	"T",
+	"U",
+	"V",
+	"W",
+	"X",
+	"Y",
+	"Z",
+	"a",
+	"b",
+	"c",
+	"d",
+	"e",
+	"f",
+	"g",
+	"h",
+	"i",
+	"j",
+	"k",
+	"l",
+	"m",
+	"n",
+	"o",
+	"p",
+	"q",
+	"r",
+	"s",
+	"t",
+	"u",
+	"v",
+	"w",
+	"x",
+	"y",
+	"z",
+	"0",
+	"1",
+	"2",
+	"3",
+	"4",
+	"5",
+	"6",
+	"7",
+	"8",
+	"9",
+	"~",
+	"`",
+	"!",
+	"@",
+	"#",
+	"$",
+	"%",
+	"^",
+	"&",
+	"*",
+	"(",
+	")",
+	"_",
+	"-",
+	"+",
+	"=",
+	"{",
+	"[",
+	"}",
+	"]",
+	",",
+	"|",
+	":",
+	";",
+	"<",
+	">",
+	".",
+	"?",
+	"/",
+]
 
 const myPopUpOne = document.getElementById("myPopUpOne")
+const firstPassword = document.getElementById("field-text-one")
+const secondPassword = document.getElementById("field-text-two")
+const passwordLength = 15
 
-const firstPassword = document.getElementById("field-text-one");
-const secondPassword = document.getElementById("field-text-two");
-let password = false;
-
-function generate () {
-    if(password === false) {
-        generatePasswords();
-    }
+function getPasswords() {
+	firstPassword.textContent = generatePasswords()
+	secondPassword.textContent = generatePasswords()
 }
 
+// generatePasswords() creates the ramdon passwords
 function generatePasswords() {
-
-        for (let i = 0; i < 15; i++) {
-            let randomIndexOne = Math.floor(Math.random()*characters.length);
-            let randomIndexTwo = Math.floor(Math.random()*characters.length);      
-            passwordOne.push(characters[randomIndexOne]);
-            passwordTwo.push(characters[randomIndexTwo]);
-        }
-        for (let i = 0; i < passwordOne.length; i++) {
-            firstPassword.textContent += passwordOne[i];
-            secondPassword.textContent += passwordTwo[i];
-        }
-        
-    password = true;
+	let password = ""
+	for (let i = 0; i < passwordLength; i++) {
+		password += getRandomCharacter()
+	}
+	return password
 }
 
-function refresh() {
-    
-    if(password === true) {
-        firstPassword.textContent = "";
-        secondPassword.textContent = "";
-        passwordOne = [];
-        passwordTwo = [];
-    }
-    password = false;
+// getRandomCharacter() generates a string of 15 random characters
+function getRandomCharacter() {
+	let randomIndex = Math.floor(Math.random() * characters.length)
+	return characters[randomIndex]
 }
 
+// The functions copyOne() and copyTwo() copy the passwords
 function copyOne(that) {
-    const inputEl = document.createElement('input');
-    inputEl.value = that.textContent
-    document.body.appendChild(inputEl)
-    inputEl.select();
-    document.execCommand('copy',false);
-    inputEl.remove();
-    myPopUpOne.classList.toggle('show');
+	const inputEl = document.createElement("input")
+	inputEl.value = that.textContent
+	document.body.appendChild(inputEl)
+	inputEl.select()
+	document.execCommand("copy", false)
+	inputEl.remove()
+	myPopUpOne.classList.toggle("show")
 }
 
 function copyTwo(that) {
-    const inputEl = document.createElement('input');
-    inputEl.value = that.textContent
-    document.body.appendChild(inputEl)
-    inputEl.select();
-    document.execCommand('copy',false);
-    inputEl.remove();
-    myPopUpTwo.classList.toggle('show');
+	const inputEl = document.createElement("input")
+	inputEl.value = that.textContent
+	document.body.appendChild(inputEl)
+	inputEl.select()
+	document.execCommand("copy", false)
+	inputEl.remove()
+	myPopUpTwo.classList.toggle("show")
 }
-

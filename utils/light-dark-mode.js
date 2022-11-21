f4f8253f08d8048933f74457e9e73542eff47410
PacelliV <flores.eugenio03@gmail.com>
2022-11-21 11:44:37 -0500

    refactored code

--- utils/light-dark-mode.js
+++ utils/light-dark-mode.js
@@ -1,18 +1,22 @@
 /* toggler() set the dark/light of the UI */
 
 function toggler() {
-    document.getElementById("toggler--light").classList.toggle("dark")
-    document.getElementById("toggler--dark").classList.toggle("dark")
-    document.getElementById("toggler--slider--circle").classList.toggle("dark")
-    document.getElementById("toggler--slider").classList.toggle("dark")
-    document.getElementById("container").classList.toggle("dark")
-    document.getElementById("logo").classList.toggle("dark")
-    document.getElementById("section-title").classList.toggle("dark")
-    document.getElementById("instruction").classList.toggle("dark")
-    document.getElementById("section-subtitle").classList.toggle("dark")
-    document.getElementById("container-passwords").classList.toggle("dark")
-    document.getElementById("pop-up-text-one").classList.toggle("dark")
-    document.getElementById("pop-up-text-two").classList.toggle("dark")
+    const idArr = [
+        "toggler--light",
+        "toggler--dark",
+        "toggler--slider--circle",
+        "toggler--slider",
+        "container",
+        "logo",
+        "section-title",
+        "section-subtitle",
+        "instruction",
+        "container-passwords",
+        "pop-up-text-one",
+        "pop-up-text-two",
+    ]
+
+    idArr.forEach((id) => document.getElementById(id).classList.toggle("dark"))
 }
 
 export default toggler
